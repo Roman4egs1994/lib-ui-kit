4882f8f41602d18813205052a9f6539ce35eab4c
Roman <r.rybkin94@gmail.com>
2024-03-17 18:47:44 +0500

    update name and version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+      name: 'ui-kit-projects',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // external: ['react', 'react-dom'],
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+      ],
+      output: { dir: 'dist', entryFileNames: '[name].js', format: 'es' },
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
 })
